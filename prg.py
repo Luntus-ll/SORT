295568d6d9809e90428d437a11586a1d39de8165
Luntus-ll <neveykovlad0501@gmail.com>
2025-10-23 12:01:44 +0300

    Add files via upload

---
+++ prg.py
@@ -0,0 +1,89 @@
+
+import time
+import random
+
+def bubble(arr):
+    st = time.time()
+    n = len(arr)
+    arr = arr.copy()
+    comparisons = 0
+    swaps = 0
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            comparisons += 1
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swaps += 1
+    elapsed_time = time.time() - st
+    return arr, comparisons, swaps, elapsed_time
+
+def select(arr):
+    st = time.time()
+    n = len(arr)
+    arr = arr.copy()
+    comparisons = 0
+    swaps = 0
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            comparisons += 1
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        if min_idx != i:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+            swaps += 1
+    elapsed_time = time.time() - st
+    return arr, comparisons, swaps, elapsed_time
+
+
+def insert(arr):
+    st = time.time()
+    arr = arr.copy()
+    comparisons = 0
+    swaps = 0
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0:
+            comparisons += 1
+            if arr[j] > key:
+                arr[j + 1] = arr[j]
+                swaps += 1
+                j -= 1
+            else:
+                break
+        arr[j + 1] = key
+    elapsed_time = time.time() - st
+    return arr, comparisons, swaps, elapsed_time
+
+def generate(size):
+    ascending = list(range(1, size + 1))
+    descending = list(range(size, 0, -1))
+    random_arr = [random.randint(1, 1000) for _ in range(size)]
+    return ascending, descending, random_arr
+
+def main():
+    size = 10
+    ascending, descending, random_array = generate(size)
+    test_arr = {
+        "По возрастанию": ascending,
+        "По убыванию": descending,
+        "Случайный": random_array
+    }
+
+    print(f"{'Массив':15} | {'Алгоритм':15} | {'Отсортированный':15} | {'Сравнения':15} | {'Перестановки':15} | {'Время, с':15}")
+
+    algorithms = [
+        ("Bubble Sort", bubble),
+        ("Selection Sort", select),
+        ("Insertion Sort", insert)
+    ]
+
+    for description, array in test_arr.items():
+        for name, func in algorithms:
+            array_copy = array.copy()
+            sorted_arr, comp, swaps, elapsed_time = func(array_copy)
+            print(f"{description:20} | {name:12} | {sorted_arr} | {comp:10} | {swaps:12} | {elapsed_time:.6f}")
+
+if __name__ == "__main__":
+    main()
